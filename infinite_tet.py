46a9052669231b8d30775479b89a54325a84b426
OceanNuclear <annoyingamygdala@gmail.com>
2020-05-17 21:20:37 +0100

    added comments

--- infinite_tet.py
+++ infinite_tet.py
@@ -5,7 +5,7 @@ import numpy as np
 
 if __name__=='__main__':
     x = np.linspace(0, e**(1/e), 1000)
-    plt.plot(x, [tet(i, 5000) for i in x], alpha=0.75, label='b = even')
+    plt.plot(x, [tet(i, 5000) for i in x], alpha=0.75, label='b = even') # 5000 is close to being infinite
     plt.plot(x, [tet(i, 5001) for i in x], alpha=0.75, label='b = odd')
     plt.title(r'$y=\lim_{b\rightarrow\infty} (a\uparrow\uparrow b)$')
     plt.xlabel(r'$a$, where $a \in [0, e^{\frac{1}{e}}]$')
